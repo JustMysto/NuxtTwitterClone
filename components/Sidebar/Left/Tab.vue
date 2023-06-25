ba0c3ad3137d4bb15b7549d1576878b6ef9b2fe1
JustMysto <xxtouchtv@gmail.com>
2023-06-25 17:22:14 +0200

    init

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<nuxt-link to="#" class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full" :class="defaultTransition">
+			<div class="w-6 h-6 text-black dark:hover:bg-dim-200 dark:text-white" :class="defaultTransition">
+				<slot name="icon"></slot>
+			</div>
+
+			<div class="ml-4 text-xl hidden xl:block dark:text-white" :class="textClasses">
+				<slot name="name"></slot>
+			</div>
+			{{ count }}
+		</nuxt-link>
+	</div>
+</template>
+
+<script setup>
+const { defaultTransition } = useTailwindConfig()
+
+const props = defineProps({
+	active: {
+		type: Boolean,
+		default: false
+	}
+})
+
+const textClasses = computed(() => props.active ? 'font-semibold' : 'font-normal')
+</script>
---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="flex flex-col">
+		<!-- Preview Card : Whats Happening -->
+		<SidebarRightPreviewCard title="Whats Happening">
+			<SidebarRightPreviewCardItem v-for="whatsHappening in whatsHappeningItems">
+				<div>
+					<h2 class="font-bold text-gray-800 text-md dark:text-white">{{ whatsHappening.title }}</h2>
+					<p class="text-xs text-gray-400">{{ whatsHappening.count }}</p>
+				</div>
+			</SidebarRightPreviewCardItem>
+		</SidebarRightPreviewCard>
+
+
+		<!-- Preview Card : Who to Follow -->
+		<SidebarRightPreviewCard title="Who to Follow">
+			<SidebarRightPreviewCardItem  v-for="whoToFollow in whoToFollowItems">
+				<div class="flex flex-row items-center justify-between p-2">
+					<div class="flex flex-row">
+						<img class="w-10 h-10 rounded-full" :src="whoToFollow.image" :alt="whoToFollow.name">
+						
+						<div class="flex flex-col ml-2">
+							<h1 class="text-sm font-bold text-gray-900 dark:text-white">{{ whoToFollow.name }}</h1>
+							<p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+						</div>
+					</div>
+					<div class="flex h-full">
+						<button class="px-4 py-2 font-bold text-xs text-white dark:text-black bg-black dark:bg-white rounded-full">Follow</button>
+					</div>
+				</div>
+			</SidebarRightPreviewCardItem>
+		</SidebarRightPreviewCard>
+
+	</div>
+</template>
+
+<script setup>
+const whatsHappeningItems = ref([
+	{
+		title: '#SpaceX',
+		count: '18.8k Tweets'
+	},
+	{
+		title: '#CodingIsFun',
+		count: '18.8k Tweets'
+	},
+	{
+		title: '#artforall',
+		count: '18.8k Tweets'
+	}
+])
+
+const whoToFollowItems = ref([
+	{
+		name: 'Joe Biden',
+		handle: '@joeBiden',
+		image: 'https://picsum.photos/200/200'
+	},
+	{
+		name: 'Joe Biden',
+		handle: '@joeBiden',
+		image: 'https://picsum.photos/200/200'
+	},
+	{
+		name: 'Joe Biden',
+		handle: '@joeBiden',
+		image: 'https://picsum.photos/200/200'
+	}
+])
+</script>
